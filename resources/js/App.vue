cc190602805b8537d99d2ed271d41294d553eca2
DuongLE <81737419+duongletocca-net@users.noreply.github.com>
2021-11-16 18:34:10 +0900

    Route set

---
+++ resources/js/App.vue
@@ -0,0 +1,42 @@
+<template>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarColor03">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <router-link to="/" class="navbar-brand">
+                            <span>Home</span>
+                        </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/products" class="navbar-brand">
+                            <span>Products</span>
+                        </router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link to="/about" class="navbar-brand">
+                            <span>About Us</span>
+                        </router-link>
+                    </li>
+                </ul>
+            </div>
+            <div class="btn btn-outline-success my-2 my-sm-0">Pick Items (0)</div>
+        </div>
+    </nav>
+    <div class="logo">
+    </div>
+</header>
+<main>
+    <div class="container">
+        <router-view />
+    </div>
+</main>
+</template>
+<style scoped>
+    .logo {
+        width: auto;
+        height: 20em;
+        background: powderblue;
+    }
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-require('./bootstrap');
+import { createApp } from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
+const app = createApp(App)
+app.use(store)
+app.use(router)
+app.mount("#app")
